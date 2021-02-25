a0b711601560d32bc57285454a4f9e45a3ed5558
Vaynbaum <78900834+Vaynbaum@users.noreply.github.com>
2021-02-25 14:37:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,8 +2,8 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
-    while(a!=0 && b!=0)
-        if(a>b)
+    while( a!= 0 && b!= 0)
+        if( a>b)
             a%=b;
         else
             b%=a;
