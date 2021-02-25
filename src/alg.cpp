45041156b55a0d3b864ddbb4f746db2f2c841e77
Vaynbaum <78900834+Vaynbaum@users.noreply.github.com>
2021-02-25 14:58:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while ( a!= 0 && b!= 0)
-        if ( a> b)
-            a%=b;
-        else
-            b%a;
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
     return a+b;
 }
